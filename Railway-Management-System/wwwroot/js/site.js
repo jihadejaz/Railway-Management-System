25771c38f479026d70507118fc321f3562457e1d
jihadejaz <jihadejaz123@gmail.com>
2023-10-10 00:34:48 +0500

    created home page and few changes left

--- Railway-Management-System/wwwroot/js/site.js
+++ Railway-Management-System/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+<script>
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+</script>
