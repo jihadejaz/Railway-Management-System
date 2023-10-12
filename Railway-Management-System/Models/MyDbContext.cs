ec2dd3245d90576229dc2b1616bae008727b9c3a
jihadejaz <jihadejaz123@gmail.com>
2023-10-12 23:53:50 +0500

    database tables without data

--- Railway-Management-System/Models/MyDbContext.cs
+++ Railway-Management-System/Models/MyDbContext.cs
@@ -4,11 +4,17 @@ namespace Railway_Management_System.Models
 {
     public class MyDbContext : DbContext
     {
-        public MyDbContext(DbContextOptions<MyDbContext> dbContext) : base(dbContext)
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
-
         }
-        public DbSet<RMSpassengers> passengers { get; set; }
 
+        public DbSet<RMSpassengers> Passengers { get; set; }
+        public DbSet<trainMaster> TrainMasters { get; set; }
+        public DbSet<stationMaster> StationMasters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define any additional configurations here, such as relationships, indexes, etc.
+        }
     }
 }
---
+++ Railway-Management-System/Models/stationMaster.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Railway_Management_System.Models
+{
+    public class stationMaster
+    {
+        [Key]
+        public int stationId { get; set; }
+        public int stationCode { get; set; }
+        public string stationName { get; set; }
+        public string stationDivision { get; set; }
+
+        [InverseProperty("stationMaster")] // Define the inverse navigation property
+        public List<trainMaster> TrainMasters { get; set; }
+    }
+}
